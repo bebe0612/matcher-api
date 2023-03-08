f698f752ceb8b8cb584b1803a8b80450b6f48002
bebe0612 <leebebe0612@gmail.com>
2023-03-08 09:19:50 +0900

    initial commit

---
+++ src/main/java/com/kw/matcher/MatcherApplication.java
@@ -0,0 +1,13 @@
+package com.kw.matcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kw/matcher/MatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kw.matcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
