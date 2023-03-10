c079764c8be0f15bf72c2502d578fdade281a667
bebe0612 <leebebe0612@gmail.com>
2023-03-10 17:25:01 +0900

    feat : Add required dependencies and Adjust app profile
    also I fixed application package first name `kr`

--- src/main/java/kr/kw/matcher/MatcherApplication.java
+++ src/main/java/kr/kw/matcher/MatcherApplication.java
@@ -1,4 +1,4 @@
-package com.kw.matcher;
+package kr.kw.matcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/kr/kw/matcher/MatcherApplicationTests.java
+++ src/test/java/kr/kw/matcher/MatcherApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kw.matcher;
+package kr.kw.matcher;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
