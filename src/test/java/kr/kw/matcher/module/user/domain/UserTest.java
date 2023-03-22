c9893b795ad39108874e3822892f5f9d69d291ae
bebe0612 <leebebe0612@gmail.com>
2023-03-22 09:42:01 +0900

    test : Test `User` entity & repository

---
+++ src/test/java/kr/kw/matcher/module/user/domain/UserTest.java
@@ -0,0 +1,36 @@
+package kr.kw.matcher.module.user.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UserTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void ofTest() {
+        User user = User.of(1L);
+
+        assertEquals(1L, user.getId());
+        assertEquals(false, user.getDeleted());
+        assertNotNull(user.getCreatedDt());
+    }
+
+    @Test
+    void saveTest() {
+        User user = User.of(1L);
+
+        User saved = userRepository.save(user);
+        Optional<User> found = userRepository.findById(1L);
+
+        assertTrue(found.isPresent());
+        assertEquals(saved.getId(), found.get().getId());
+    }
+}
