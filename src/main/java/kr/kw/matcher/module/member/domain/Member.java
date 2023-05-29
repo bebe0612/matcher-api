e1808a261eed6f86265040e511b36764e39e6904
James Lee <leebebe0612@gmail.com>
2023-05-30 08:53:51 +0900

    feat : Add `Member` class

---
+++ src/main/java/kr/kw/matcher/module/member/domain/Member.java
@@ -0,0 +1,78 @@
+package kr.kw.matcher.module.member.domain;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Builder
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "member")
+public class Member implements UserDetails {
+    private static final long serialVersionUID = 493769019926722502L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return id.toString();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public static Member ofAdmin() {
+        return Member.builder()
+                .roles(List.of("ROLE_ADMIN"))
+                .build();
+    }
+
+    public static Member ofUser() {
+        return Member.builder()
+                .roles(List.of("ROLE_USER"))
+                .build();
+    }
+}
