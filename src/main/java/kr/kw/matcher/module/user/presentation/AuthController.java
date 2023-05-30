4f9481fb9247982c899125ebe64223bd67c24f08
James Lee <leebebe0612@gmail.com>
2023-05-30 09:27:21 +0900

    remove : Remove signIn feature

--- src/main/java/kr/kw/matcher/module/user/presentation/AuthController.java
+++ src/main/java/kr/kw/matcher/module/user/presentation/AuthController.java
@@ -2,11 +2,9 @@ package kr.kw.matcher.module.user.presentation;
 
 import kr.kw.matcher.module.user.application.AuthService;
 import kr.kw.matcher.module.user.application.dto.AuthDto;
-import kr.kw.matcher.module.user.presentation.request.SignInEmailBody;
 import kr.kw.matcher.module.user.presentation.request.SignUpBody;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,13 +22,5 @@ public class AuthController {
     @PostMapping(value = "/signup")
     public AuthDto signUp(@RequestBody @Valid SignUpBody body) {
         return authService.signUp(body);
-    }
-
-    @PostMapping(value = "/signin/email")
-    public AuthDto signInWithEmail(@RequestBody @Valid SignInEmailBody body) {
-        return AuthDto.builder()
-                .token("token")
-                .userId(1L)
-                .build();
     }
 }
