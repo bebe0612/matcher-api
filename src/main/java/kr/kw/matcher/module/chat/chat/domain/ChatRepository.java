0672d444eecfa26336ca0fd951f582fc6ff1fc35
Andrew <superhsk00@gmail.com>
2023-06-04 22:59:38 +0900

    Revert "Revert "+특정 유저가 포함된 채팅방 찾기""
    This reverts commit 5e94f109366355bf822c10af806fad2205b5efd8.

--- src/main/java/kr/kw/matcher/module/chat/chat/domain/ChatRepository.java
+++ src/main/java/kr/kw/matcher/module/chat/chat/domain/ChatRepository.java
@@ -1,6 +1,11 @@
 package kr.kw.matcher.module.chat.chat.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRepository extends JpaRepository<Chat, Long> {
+    Page<Chat> findByRoomId(Long roomId, Pageable pageable);
 }
