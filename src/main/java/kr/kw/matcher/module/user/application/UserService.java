c5959d76e7ede8f1ad0531b3483541bb680f9b92
bebe0612 <leebebe0612@gmail.com>
2023-09-27 18:14:34 +0900

    feat: Add GET my profile

--- src/main/java/kr/kw/matcher/module/user/application/UserService.java
+++ src/main/java/kr/kw/matcher/module/user/application/UserService.java
@@ -1,6 +1,9 @@
 package kr.kw.matcher.module.user.application;
 
 
+import kr.kw.matcher.core.exception.NotFoundException;
+import kr.kw.matcher.module.user.application.dto.UserDto;
+import kr.kw.matcher.module.user.domain.User;
 import kr.kw.matcher.module.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +14,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public UserDto getMyProfile(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(NotFoundException::new);
+
+        return UserDto.from(user);
+    }
 
     public String getUserNickname(Long userId) {
         return userRepository.getReferenceById(userId).getNickname();
