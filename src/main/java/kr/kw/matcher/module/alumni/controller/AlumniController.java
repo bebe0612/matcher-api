5ede388ed9230f4d355f0b5d15d44712e0e0b020
bebe0612 <leebebe0612@gmail.com>
2023-09-27 17:31:55 +0900

    fix: Fix principal not load user info error

--- src/main/java/kr/kw/matcher/module/alumni/controller/AlumniController.java
+++ src/main/java/kr/kw/matcher/module/alumni/controller/AlumniController.java
@@ -1,10 +1,12 @@
 package kr.kw.matcher.module.alumni.controller;
 
 import kr.kw.matcher.module.alumni.service.AlumniService;
+import kr.kw.matcher.module.member.domain.Member;
 import kr.kw.matcher.module.user.application.dto.UserDto;
 import kr.kw.matcher.module.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,16 +18,12 @@ public class AlumniController {
     private final AlumniService alumniService;
 
     @GetMapping
-    public List<UserDto> alumni(Authentication authentication) { // 전체 동창 리스트
-        User user = (User) authentication.getPrincipal();
-
-        return alumniService.getRecentAlumni(user.getId());
+    public List<UserDto> alumni(@AuthenticationPrincipal Member member) { // 전체 동창 리스트
+        return alumniService.getRecentAlumni(member.getId());
     }
 
     @PostMapping("/{alumniId}")
-    public void addFriend(Authentication authentication, @PathVariable Long alumniId) { // 해당 동창을 친구로 추가
-        User user = (User) authentication.getPrincipal();
-
-        alumniService.addFriend(user.getId(), alumniId);
+    public void addFriend(@AuthenticationPrincipal Member member, @PathVariable Long alumniId) { // 해당 동창을 친구로 추가
+        alumniService.addFriend(member.getId(), alumniId);
     }
 }
--- src/main/java/kr/kw/matcher/module/friend/controller/FriendController.java
+++ src/main/java/kr/kw/matcher/module/friend/controller/FriendController.java
@@ -2,9 +2,11 @@ package kr.kw.matcher.module.friend.controller;
 
 import kr.kw.matcher.module.friend.dto.FriendDto;
 import kr.kw.matcher.module.friend.service.FriendService;
+import kr.kw.matcher.module.member.domain.Member;
 import kr.kw.matcher.module.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +20,9 @@ public class FriendController {
     private final FriendService friendService;
 
     @GetMapping
-    public List<FriendDto> friends(Authentication authentication) { // 전체 친구 조회
-        User user = (User) authentication.getPrincipal();
+    public List<FriendDto> friends(@AuthenticationPrincipal Member member) { // 전체 친구 조회
 
-        return friendService.getFriends(user.getId());
+        return friendService.getFriends(member.getId());
     }
 
 }
