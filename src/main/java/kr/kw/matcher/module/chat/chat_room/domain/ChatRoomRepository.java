26c76934043a3f618626a5c2291145d124a58e49
bebe0612 <leebebe0612@gmail.com>
2023-10-06 14:53:05 +0900

    feat: Add room id to FriendDto

--- src/main/java/kr/kw/matcher/module/chat/chat_room/domain/ChatRoomRepository.java
+++ src/main/java/kr/kw/matcher/module/chat/chat_room/domain/ChatRoomRepository.java
@@ -2,5 +2,8 @@ package kr.kw.matcher.module.chat.chat_room.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    Optional<ChatRoom> findByHostIdAndFriendId(Long hostId, Long friendId);
 }
